0c4ff1a5b1a414928947bd20d5d9028b3819a2ba
Will <williamjamescook@aol.com>
2021-03-26 17:02:52 +0000

    implement theme select

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
-import About from "./Components/About";
-import Projects from "./Components/Projects";
-import Contact from "./Components/Contact";
 
 function App() {
 	return (
 		<div className="App">
 			<Dashboard />
-			<About />
-			<Projects />
-			<Contact />
 		</div>
 	);
 }
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import computer from "../images/Computer_icon.png";
 import hair from "../images/Hair.png";
 import me from "../images/me.jpg";
@@ -7,11 +7,102 @@ import github from "../images/Github_logo.png";
 import email from "../images/Email.png";
 import { ExternalLink } from "react-external-link";
 import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 export default function Dashboard() {
+	const [about, setAbout] = useState(true);
+	const [projects, setProjects] = useState(false);
+	const [contact, setContact] = useState(false);
+	const [blueButton, setBlue] = useState(true);
+	const [redButton, setRed] = useState(false);
+	const [greenButton, setGreen] = useState(false);
+	const [pinkButton, setPink] = useState(false);
+
+	const aboutClick = () => {
+		setAbout(true);
+		setProjects(false);
+		setContact(false);
+	};
+
+	const projectsClick = () => {
+		setProjects(true);
+		setAbout(false);
+		setContact(false);
+	};
+	const contactClick = () => {
+		setContact(true);
+		setAbout(false);
+		setProjects(false);
+	};
+
+	const pageSelect = () => {
+		if (about) {
+			return <About />;
+		} else if (projects) {
+			return <Projects />;
+		} else if (contact) {
+			return <Contact />;
+		}
+	};
+
+	const blueClick = () => {
+		setBlue(true);
+		setRed(false);
+		setGreen(false);
+		setPink(false);
+	};
+	const redClick = () => {
+		setRed(true);
+		setBlue(false);
+		setGreen(false);
+		setPink(false);
+	};
+	const greenClick = () => {
+		setGreen(true);
+		setRed(false);
+		setBlue(false);
+		setPink(false);
+	};
+	const pinkClick = () => {
+		setPink(true);
+		setRed(false);
+		setGreen(false);
+		setBlue(false);
+	};
+
+	const themeSelect = () => {
+		if (blueButton) {
+			return "proBlue";
+		}
+		if (redButton) {
+			return "proRed";
+		}
+		if (greenButton) {
+			return "proGreen";
+		}
+		if (pinkButton) {
+			return "proPink";
+		}
+	};
+	const backgroundThemeSelect = () => {
+		if (blueButton) {
+			return "blue";
+		}
+		if (redButton) {
+			return "red";
+		}
+		if (greenButton) {
+			return "green";
+		}
+		if (pinkButton) {
+			return "pink";
+		}
+	};
+
 	return (
 		<>
-			<main>
+			<main className={backgroundThemeSelect()}>
 				<section className="glass">
 					<div className="dashboard">
 						<div className="user">
@@ -25,13 +116,25 @@ export default function Dashboard() {
 							<div className="link">
 								<img src={hair} alt="Glasses icon" />
 								<h2>
-									<button>About Me</button>
+									<button
+										onClick={() => {
+											aboutClick();
+										}}
+									>
+										About Me
+									</button>
 								</h2>
 							</div>
 							<div className="link">
 								<img src={computer} alt="Computer icon" />
 								<h2>
-									<button>Projects</button>
+									<button
+										onClick={() => {
+											projectsClick();
+										}}
+									>
+										Projects
+									</button>
 								</h2>
 							</div>
 							<div className="link">
@@ -53,21 +156,55 @@ export default function Dashboard() {
 							<div className="link">
 								<img src={email} alt="Email icon" />
 								<h2>
-									<button>Contact Me</button>
+									<button
+										onClick={() => {
+											contactClick();
+										}}
+									>
+										Contact Me
+									</button>
 								</h2>
 							</div>
 						</div>
-						<div className="pro">
+						<div className={themeSelect()}>
 							<h2>Select a theme</h2>
 							<div id="theme-options-wrapper">
-								<div data-mode="blue" id="blue-mode" className="theme-dot"></div>
-								<div data-mode="pink" id="pink-mode" className="theme-dot"></div>
-								<div data-mode="red" id="red-mode" className="theme-dot"></div>
-								<div data-mode="green" id="green-mode" className="theme-dot"></div>
+								<div
+									data-mode="blue"
+									id="blue-mode"
+									className="theme-dot"
+									onClick={() => {
+										blueClick();
+									}}
+								></div>
+								<div
+									data-mode="pink"
+									id="pink-mode"
+									className="theme-dot"
+									onClick={() => {
+										pinkClick();
+									}}
+								></div>
+								<div
+									data-mode="red"
+									id="red-mode"
+									className="theme-dot"
+									onClick={() => {
+										redClick();
+									}}
+								></div>
+								<div
+									data-mode="green"
+									id="green-mode"
+									className="theme-dot"
+									onClick={() => {
+										greenClick();
+									}}
+								></div>
 							</div>
 						</div>
 					</div>
-					<About />
+					{pageSelect()}
 				</section>
 			</main>
 			<div class="circle1"></div>
